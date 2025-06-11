9f3615317e049405d1a304a5aec086778af28042
Fahadn99 <fahad97@outlook.com>
2025-06-11 07:32:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const websites = {
 "1":"https://maps.app.goo.gl/KiKgAYwhPcow5itx7",
+"QM4-172-71-53-4":"https://maps.app.goo.gl/dETZXwCWVT6bnNEn6?g_st=ic",
 "Qm4-172-71-107-22":"https://maps.app.goo.gl/tEjdJs3UdftSZ6p27?g_st=ic",
 "QM9-102":"https://maps.app.goo.gl/uBztLAQMTMByHdHD6?g_st=ic",
 "MB1-16":"https://maps.app.goo.gl/8PbkQShvJJq2kANd9?g_st=ic",
